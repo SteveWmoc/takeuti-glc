6564d62c269a25190a3439f7184af8375997b940
Steven Sabean <steves1962wmoc@gmail.com>
2026-08-16 19:11:18 -0500

    Expose opening and closing experiments

--- TakeutiGLC.lean
+++ TakeutiGLC.lean
@@ -2,4 +2,6 @@ import TakeutiGLC.Syntax.TypeProfile
 import TakeutiGLC.Syntax.Symbol
 import TakeutiGLC.Experiment.Names
 import TakeutiGLC.Experiment.Binding.DeBruijn
+import TakeutiGLC.Experiment.Binding.DeBruijnOps
 import TakeutiGLC.Experiment.Binding.LocallyNameless
+import TakeutiGLC.Experiment.Binding.LocallyNamelessOps
