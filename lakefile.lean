7a1e9c13c267f3fd84d8928b617928bfa08815a3
Steven Sabean <steves1962wmoc@gmail.com>
2026-09-03 12:18:51 -0500

    Upgrade mathlib to 4.33.1

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package «takeuti-glc» where
   version := v!"0.1.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.32.1"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.1"
 
 @[default_target]
 lean_lib TakeutiGLC
