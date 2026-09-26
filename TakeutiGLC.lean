77d8dcd6728c88b079eb32269aca114d1f3375e4
Steven Sabean <steven@ceadmath.org>
2026-09-26 10:49:40 -0500

    Export hereditary instantiation

--- TakeutiGLC.lean
+++ TakeutiGLC.lean
@@ -9,6 +9,7 @@ import TakeutiGLC.Syntax.OpenClose
 import TakeutiGLC.Syntax.Renaming
 import TakeutiGLC.Syntax.SelectedClosing
 import TakeutiGLC.Syntax.Instantiation
+import TakeutiGLC.Syntax.HereditaryInstantiation
 import TakeutiGLC.Syntax.Substitution
 import TakeutiGLC.Experiment.Names
 import TakeutiGLC.Experiment.Binding.DeBruijn
